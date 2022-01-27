2de65be767dfbfcffecaf1f5fe8bd58172fe9c81
ranog <jprnogueira@yahoo.com.br>
2022-01-26 23:16:29 -0300

    using SQLAlchemy directly in our API endpoint

--- src/entrypoints/allocate_endpoint.py
+++ src/entrypoints/allocate_endpoint.py
@@ -1,14 +1,31 @@
+import flask
 from urllib import request
-from src.domain.model import OrderLine, allocate
+
+from src.domain.model import Batch, OrderLine, allocate
+
+
+def start_session():
+    pass
 
 
 @flask.route.gubbins
 def allocate_endpoint():
+    session = start_session()
+
     # extract order line from request
-    line = OrderLine(request.params, ...)
+    line = OrderLine(
+        request.json['orderid'],
+        request.json['sku'],
+        request.json['qty'],
+    )
+
     # load all batches from the DB
-    batches = ...
+    batches = session.query(Batch).all()
+
     # call our domain service
     allocate(line, batches)
-    # then save the allocation back to the database somehow
+
+    # save the allocation back to the database
+    session.commit()
+
     return 201
