c3bb72d79d514655213f55c79547e20965bf0a9d
ranog <jprnogueira@yahoo.com.br>
2022-02-27 20:09:43 -0300

    dataclasses are great for value objects

--- value_objects.py
+++ value_objects.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
-from typing import NamedTuple
 from collections import namedtuple
+
+import pytest
 
 
 @dataclass(frozen=True)
@@ -9,9 +10,15 @@ class Name:
     surname: str
 
 
-class Money(NamedTuple):
+@dataclass(frozen=True)
+class Money:
     currency: str
     value: int
+
+    def __add__(self, other):
+        if other.currency != self.currency:
+            raise ValueError(f'Cannot add {self.currency} to {other.currency}')
+        return Money(self.currency, self.value + other.value)
 
 
 Line = namedtuple('Line', ['sku', 'qty'])
@@ -21,3 +28,29 @@ def test_equality():
     assert Money(currency='gbp', value=10)
     assert Name(first_name='Harry', surname='Percival') != Name(first_name='Bob', surname='Gregory')
     assert Line(sku='RED-CHAIR', qty=5) == Line(sku='RED-CHAIR', qty=5)
+
+
+fiver = Money(currency='gbp', value=5)
+tenner = Money(currency='gbp', value=10)
+
+
+def test_can_add_money_values_for_the_same_currency():
+    assert fiver.value + fiver.value == tenner.value
+
+
+def test_can_subtract_money_values():
+    assert tenner.value - fiver.value == fiver.value
+
+
+def test_adding_different_currencies_fails():
+    with pytest.raises(ValueError):
+        Money(currency='usd', value=10) + Money(currency='gbp', value=10)
+
+
+def test_can_multiply_money_by_a_number():
+    assert fiver.value * 5 == Money(currency='gbp', value=25).value
+
+
+def test_multiplying_two_money_values_is_an_error():
+    with pytest.raises(TypeError):
+        tenner * fiver
