18b6fe27c5258381c636093b6ef2aae65c236dc0
ranog <jprnogueira@yahoo.com.br>
2022-03-12 18:51:55 -0300

    explicit ORM mapping with SQLAlchemy Table objects

--- src/allocation/adapters/orm.py
+++ src/allocation/adapters/orm.py
@@ -5,6 +5,8 @@ from src.allocation.domain import model
 metadata = MetaData()
 
 order_lines = Table(
+    'order_lines',
+    metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('sku', String(250)),
     Column('qty', Integer, nullable=False),
