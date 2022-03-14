000a69141772b583acfd4de424153d99ec9b4e80
ranog <jprnogueira@yahoo.com.br>
2022-03-13 22:11:35 -0300

    Testing the ORM directly (throwaway tests)

---
+++ tests/unit/adapters/test_orm.py
@@ -0,0 +1,26 @@
+from src.allocation.domain import model
+
+
+def test_orderline_mapper_can_load_lines(session):
+    session.execute(
+        "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+        '("order1", "RED-CHAIR", 12),'
+        '("order1", "RED-TABLE", 13),'
+        '("order2", "BLUE-LIPSTICK", 14)'
+    )
+    expected = [
+        model.OrderLine(orderid='order1', sku='RED-CHAIR', qty=12),
+        model.OrderLine(orderid='order1', sku='RED-TABLE', qty=13),
+        model.OrderLine(orderid='order2', sku='BLUE-LIPSTICK', qty=14),
+    ]
+
+    assert session.query(model.OrderLine).all() == expected
+
+
+def test_orderline_mapper_can_save_lines(session):
+    new_line = model.OrderLine(orderid='order1', sku='DECORATIVE-WIDGET', qty=12)
+    session.add(new_line)
+    session.commit()
+    rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
+
+    assert rows == [('order1', 'DECORATIVE-WIDGET', 12)]
