b4edf5c41b0d7e7a11d576034cf34f3de74f53d9
ranog <jprnogueira@yahoo.com.br>
2022-03-14 22:03:25 -0300

    the simplest possible repository

---
+++ src/allocation/adapters/repository.py
@@ -0,0 +1,12 @@
+from src.allocation.domain import model
+import abc
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
