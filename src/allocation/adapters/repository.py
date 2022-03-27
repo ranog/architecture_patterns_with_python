b1c3b1624e0485bb078c185b4d6159961c5d612f
ranog <jprnogueira@yahoo.com.br>
2022-03-27 20:16:08 -0300

    Repository test for saving an object

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -1,12 +1,22 @@
-from src.allocation.domain import model
+from src.allocation.domain.model import Batch
 import abc
 
 
 class AbstractRepository(abc.ABC):
+
     @abc.abstractmethod
-    def add(self, batch: model.Batch):
+    def add(self, batch: Batch):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, reference) -> model.Batch:
+    def get(self, reference) -> Batch:
         raise NotImplementedError
+
+
+class SqlAlchemyRepository:
+
+    def __init__(self, batch: Batch) -> None:
+        self.batch = batch
+
+    def add(self, batch: Batch):
+        pass
--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,5 +1,14 @@
+from src.allocation.adapters.repository import SqlAlchemyRepository
 from src.allocation.domain.model import Batch
 
 
 def test_repository_can_save_a_batch(session):
     batch = Batch(ref='batch1', sku='RUSTY-SOAPDISH', qty=100, eta=None)
+
+    repo = SqlAlchemyRepository(session)
+    repo.add(batch)
+    session.commit()
+
+    rows = session.execute('SELECT reference, sku, _purchased_quantity, eta FROM "batches"')
+
+    assert list(rows) == [('batch1', 'RUSTY-SOAPDISH', 100, None)]
